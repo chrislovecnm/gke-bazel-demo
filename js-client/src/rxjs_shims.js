21447d52d96afa1ca90656e9da99669a5a80b4b4
chrislovecnm <chlove@google.com>
2019-05-16 12:55:35 -0600

    fixing linting

--- js-client/src/rxjs_shims.js
+++ js-client/src/rxjs_shims.js
@@ -1,15 +1,16 @@
-/**
- * @license
- * Copyright Google Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
-
- /**
-  * @fileoverview these provide named UMD modules so that we can bundle
-  * the application along with rxjs using the concatjs bundler.
-  */
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // rxjs/operators
 (function(factory) {
